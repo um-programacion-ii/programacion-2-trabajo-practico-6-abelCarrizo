8d65528d86f2c7514e9bc405d9e065515f51899a
abelCarrizo <aa.carrizo@alumno.um.edu.ar>
2025-09-19 21:12:09 -0300

    feat: añadir excepciones

---
+++ microservices-system/business-service/src/main/java/com/mycompany/app/business_service/exceptions/ValidacionNegocioException.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.business_service.exceptions;
+
+    public class ValidacionNegocioException extends RuntimeException {
+    public ValidacionNegocioException(String message) { super(message); }
+}
